8d077453df01f528a5542eedbdde3118fdb0d2fd
Bezdicekz <zdenek.bezdicek@gmail.com>
2024-09-26 06:33:11 +0200

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::view('/', 'index');
+Route::view('/dashboard', 'dashboard');
 Route::get('/login', [AuthController::class, 'show']);
