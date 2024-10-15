9789d1437eab8bcc6f5810ade40d5fc30bda9404
Zdeněk Bezdíček <zdenek.bezdicek@gmail.com>
2024-10-15 18:46:39 +0200

    modální okno editace úkolu

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -131,25 +131,13 @@
                   </div>
                   
                   <!-- Ikona edituj úkol -->
-                  <a href="{{ route('ukol.edit', $ukol->id) }}" class="btn-edit"><svg id="edituj-ukol" class="hover:text-gray-800 w-6 h-6 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                  </svg></a>
-      
-                  <!-- Ikona smaž úkol -->
-                  <form action="{{ route('ukol.destroy', $ukol->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn-delete">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hover:text-gray-800 w-6 h-6 text-gray-300"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                          </svg>
-                        </button>
-                  </form>
-
-                  <!-- Připravné modální okno -->
+                                     <!-- Připravné modální okno -->
                   <div x-data="{ modalOpen: false }"
                       @keydown.escape.window="modalOpen = false"
                       class="relative z-50 w-auto h-auto">
-                      <button @click="modalOpen=true" class="inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors bg-white border rounded-md hover:bg-neutral-100 active:bg-white focus:bg-white focus:outline-none focus:ring-2 focus:ring-neutral-200/60 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none">Open</button>
+                      <button @click="modalOpen=true" class="inline-flex items-center justify-center"><svg id="edituj-ukol" class="hover:text-gray-800 w-6 h-6 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                  </svg></button>
                       <template x-teleport="body">
                           <div x-show="modalOpen" class="fixed top-0 left-0 z-[99] flex items-center justify-center w-screen h-screen" x-cloak>
                               <div x-show="modalOpen" 
@@ -176,12 +164,74 @@
                                       </button>
                                   </div>
                                   <div class="relative w-auto">
-                                      <p>V tomto okně bude formulář pro editaci úkolu.</p>
+                                  <div class="bg-cb3 mb-4 rounded-lg shadow-lg p-4 lg:w-4/5 text-center mx-auto text-ctprim">   
+                                    <h1 class="text-xl font-bold mb-4">Upravit úkol</h1>
+                                    
+                                    <form action="{{ route('ukol.update', $ukol->id) }}" method="POST" class="flex flex-col gap-4 justify-center items-center">
+                                        @csrf
+                                        @method('PUT')
+                                        
+                                        <div>
+                                        <label for="nazev">Název úkolu:</label>
+                                        <input type="text" name="nazev" id="nazev" value="{{ $ukol->nazev }}" required>
+                                        </div>
+
+                                        <div>
+                                        <label for="popis">Popis:</label>
+                                        <textarea name="popis" id="popis" required>{{ $ukol->popis }}</textarea>
+                                        </div>
+
+                                        <div>
+                                        <label for="id_projektu">Projekt:</label>
+                                        <input type="text" name="id_projektu" id="id_projektu" value="{{ $ukol->id_projektu }}" required>
+                                        </div>
+
+                                        <div>
+                                        <label for="celkovy_cas_ukolu">Celkový čas:</label>
+                                        <input type="number" name="celkovy_cas_ukolu" id="celkovy_cas_ukolu" value="{{ $ukol->celkovy_cas_ukolu }}" required>
+                                        </div>
+
+                                        <div>
+                                        <label for="datum_zahajeni">Datum zahájení:</label>
+                                        <input type="date" name="datum_zahajeni" id="datum_zahajeni" value="{{ $ukol->datum_zahajeni }}" required>
+                                        </div>
+
+                                        <div>
+                                        <label for="planovany_datum_ukonceni">Plánovaný datum ukončení:</label>
+                                        <input type="date" name="planovany_datum_ukonceni" id="planovany_datum_ukonceni" value="{{ $ukol->planovany_datum_ukonceni }}" required>
+                                        </div>
+
+                                        <div>
+                                        <label for="stav">Stav:</label>
+                                        <input type="text" name="stav" id="stav" value="{{ $ukol->stav }}" required>
+                                        </div>
+
+                                        <div>
+                                        <label for="rozpocet">Rozpočet:</label>
+                                        <input type="number" name="rozpocet" id="rozpocet" value="{{ $ukol->rozpocet }}" required>
+                                        </div>
+
+                                        <button type="submit" class="py-2 px-6 transition duration-700 ease-in-out rounded-md hover:text-white hover:bg-cbtsec">Uložit změny</button>
+                                    </form>
+
+                                    </div>
+
                                   </div>
                               </div>
                           </div>
                       </template>
                   </div>
+                        
+                  <!-- Ikona smaž úkol -->
+                  <form action="{{ route('ukol.destroy', $ukol->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn-delete">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hover:text-gray-800 w-6 h-6 text-gray-300"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                          </svg>
+                        </button>
+                  </form>
+          
                 </div>
             </div>
             @endforeach
